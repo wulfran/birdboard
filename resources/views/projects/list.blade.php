b90b220e15bb006773bc61a4be61e046c107cdf0
wulfran <piotr.nikonowicz@gmail.com>
2019-02-27 20:41:36 +0100

    Continue with the styling

--- resources/views/projects/list.blade.php
+++ resources/views/projects/list.blade.php
@@ -2,21 +2,30 @@
 
 
 @section('content')
-    <div class="flex items-center mb-3">
-        <a href="{{ route('projects.create') }}">New Project</a>
-    </div>
+    <header class="flex items-center mb-3 py-4">
+        <div class="flex justify-between w-full items-center">
+            <h2 class="text-grey text-small font-normal">
+                My Projects
+            </h2>
+            <a class="button" href="{{ route('projects.create') }}">New Project</a>
+        </div>
+    </header>
 
-    <div class="flex">
+    <main class="lg:flex lg:flex-wrap -mx-3">
         @forelse($projects as $project)
-            <div class="bg-white mr-4 p-5 rounded shadow w-1/3" style="height: 200px;">
-                <h3 class="font-normal text-xl py-5">{{ $project->title }}</h3>
+            <div class="lg:w-1/3 px-3 pb-6">
+                <div class="bg-white p-5 rounded-lg shadow" style="height: 200px;">
+                    <h3 class="font-normal text-xl py-4 -ml-5 border-l-4 border-blue-light pl-4 mb-3">
+                        <a class="text-black no-underline" href="{{ route('projects.show', $project->id) }}">{{ $project->title }}</a>
+                    </h3>
 
-                <div class="text-grey">{{ str_limit($project->description, 100) }}</div>
+                    <div class="text-grey">{{ str_limit($project->description, 100) }}</div>
+                </div>
             </div>
         @empty
             <div>
                 No projects yet.
             </div>
         @endforelse
-    </div>
+    </main>
 @endsection
